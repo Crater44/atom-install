cc06edb0b8d59ba51a57674b61a32d2c33b589a1
crater44 <dragmos_we@protonmail.com>
2023-10-31 16:38:43 +0100

    script fix

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ deb_url="https://download.pulsar-edit.dev/?os=linux&type=linux_deb"
 deb_filename="pulsar.deb"
 download_dir="/tmp"
 custom_packages_dir=($(pwd)/atom-custom-packages)
+mkdir $custom_packages_dir
+github_url="https://github.com"
 wget -O "$download_dir/$deb_filename" "$deb_url"
 if [ $? -eq 0 ]; then
     deb_path="$download_dir/$deb_filename"
@@ -16,20 +18,24 @@ if [ $? -eq 0 ]; then
 else
     echo "Failed to download pulsar."
 fi
-mkdir $custom_packages_dir
-cd $custom_packages_dir
-git clone https://github.com/Glavin001/atom-beautify .
-git clone https://github.com/file-icons/atom .
-git clone https://github.com/emmetio/emmet-atom .
-git clone https://github.com/TypeStrong/atom-typescript .
-git clone https://github.com/Pulsar-Edit-Highlights/selected .
-git clone https://github.com/steelbrain/intentions .
-git clone https://github.com/abe33/atom-pigments .
-directories=($(ls -d */))
-for dir in "${directories[@]}"; do
-    echo "Directory: $dir"
-    cd $dir
-    ppm link .
-    ppm install
-    cd $custom_packages_dir
+
+declare -A packages=(
+    ["Glavin001"]="atom-beautify"
+    ["file-icons"]="atom"
+    ["emmetio"]="emmet-atom"
+    ["TypeStrong"]="atom-typescript"
+    ["Pulsar-Edit-Highlights"]="selected"
+    ["steelbrain"]="intentions"
+    ["abe33"]="atom-pigments"
+)
+
+for owner in "${!packages[@]}"; do
+    package=${packages[$owner]}
+    git clone $github_url/$owner/$package $custom_packages_dir/$package
 done
+
+directories=($(ls -d $custom_packages_dir/*))
+for dir in "${directories[@]}"; do
+    ppm link $custom_packages_dir/$dir
+done
+ppm install
